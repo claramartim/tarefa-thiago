4f21cb814a26c8a83d7c79b74086a3200f4cca5e
claramartim <271965@unifio.edu.br>
2025-09-16 01:39:34 +0000

    15/09

---
+++ tarefa-thiago/src/app/app-module.ts
@@ -0,0 +1,23 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Funcionario } from './dados/funcionario/funcionario';
+
+@NgModule({
+  declarations: [
+    App,
+    Funcionario
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
